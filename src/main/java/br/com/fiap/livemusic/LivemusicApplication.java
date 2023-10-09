4bb152b4b233bdd160466863c986219434a02393
isabellapiola1 <rm93960@fiap.com.br>
2023-10-09 18:38:06 -0300

    first commit

---
+++ src/main/java/br/com/fiap/livemusic/LivemusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.livemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivemusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivemusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/livemusic/LivemusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.livemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivemusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
