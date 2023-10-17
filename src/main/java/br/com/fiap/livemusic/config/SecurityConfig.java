ecacd92dd13086fd487cbd87568af41f67eba597
Isabella Piola <101204091+IsabellaPiola1@users.noreply.github.com>
2023-10-17 09:31:48 -0300

    Config

--- src/main/java/br/com/fiap/livemusic/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/livemusic/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package br.com.fiap.epictask.config;
+package br.com.fiap.livemusic.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
