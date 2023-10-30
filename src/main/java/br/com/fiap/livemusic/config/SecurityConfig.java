db9b54660381721154d95523d61a4e2b958b9f67
isabellapiola1 <rm93960@fiap.com.br>
2023-10-30 18:52:50 -0300

    service

--- src/main/java/br/com/fiap/livemusic/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/livemusic/config/SecurityConfig.java
@@ -1,20 +1,28 @@
 package br.com.fiap.livemusic.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import br.com.fiap.livemusic.user.UserRepository;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception    {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http
-            .authorizeHttpRequests( auth -> auth.anyRequest().authenticated() )
-            .oauth2Login( form -> form.loginPage("/login").defaultSuccessUrl("/task").permitAll())
-            .logout( logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login") )
-            .build();
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .oauth2Login( form -> form.loginPage("/login").defaultSuccessUrl("/task").permitAll())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login") )
+                .addFilterBefore(new LoginFilter(userRepository), OAuth2LoginAuthenticationFilter.class)
+                .build();
     }
     
 }
--- src/main/java/br/com/fiap/livemusic/task/TaskService.java
+++ src/main/java/br/com/fiap/livemusic/task/TaskService.java
@@ -1,9 +1,15 @@
 package br.com.fiap.livemusic.task;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import br.com.fiap.livemusic.user.User;
+import br.com.fiap.livemusic.user.UserService;
 
 @Service
 public class TaskService {
@@ -11,14 +17,18 @@ public class TaskService {
     @Autowired
     TaskRepository repository;
 
-    public List<Task> findAll(){
+    @Autowired
+    UserService userService;
+
+    public List<Task> findAll() {
         return repository.findAll();
     }
 
     public boolean delete(Long id) {
         var task = repository.findById(id);
 
-        if(task.isEmpty()) return false;
+        if (task.isEmpty())
+            return false;
 
         repository.deleteById(id);
         return true;
@@ -27,5 +37,86 @@ public class TaskService {
     public void save(Task task) {
         repository.save(task);
     }
-    
+
+    public void decrement(Long id) {
+        
+        var optional = repository.findById(id);
+
+        if (optional.isEmpty())
+            throw new RuntimeException("musica não encontrada");
+
+        var task = optional.get();
+
+        if (task.getStatus() == null || task.getStatus() <= 0)
+            throw new RuntimeException("musica não pode ter status negativo");
+
+        task.setStatus(task.getStatus() - 10);
+
+        // salvar
+        repository.save(task);
+    }
+
+    public void increment(Long id) {
+        
+        var optional = repository.findById(id);
+
+        if (optional.isEmpty())
+            throw new RuntimeException("musica não encontrada");
+
+        var task = optional.get();
+
+        if (task.getStatus() == null)
+            task.setStatus(0);
+
+        if (task.getStatus() == 100) {
+            throw new RuntimeException("musica não pode ter status maior que 100");
+        }
+
+        task.setStatus(task.getStatus() + 10);
+
+        if (task.getStatus() == 100){
+            var user = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            userService.addScore(User.convert(user) , task.getScore());
+        }
+
+        // salvar
+        repository.save(task);
+    }
+
+    public void cacthTask(Long id, OAuth2User user) {
+        var optional = repository.findById(id);
+
+        if (optional.isEmpty())
+            throw new RuntimeException("musica não encontrada");
+
+        var task = optional.get();
+
+        if (task.getUser() != null)
+            throw new RuntimeException("musica já atribuída");
+
+        task.setUser(User.convert(user));
+
+        repository.save(task);
+
+    }
+
+    public void dropTask(Long id, OAuth2User user) {
+        var optional = repository.findById(id);
+
+        if (optional.isEmpty())
+            throw new RuntimeException("musica não encontrada");
+
+        var task = optional.get();
+
+        if (task.getUser() == null)
+            throw new RuntimeException("musica não atribuída");
+            
+        if (!task.getUser().equals(User.convert(user)))
+            throw new RuntimeException("não pode adicionar musica de outra pessoa");
+
+        task.setUser(null);
+
+        repository.save(task);
+    }
+
 }
